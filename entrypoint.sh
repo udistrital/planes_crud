4edfd392740d4b6cc2ab4e33000bf2ecbfa59085
fabianbarreto02 <fabibeto11@hotmail.com>
2021-08-12 14:36:45 -0500

    hotfix: Ajuste de archivo entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -u
 set -o pipefail
 
 export PLANES_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/planes_mongo_crud/db/username --output text --query Parameter.Value)"
-export PLANES_CRUD_USER="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/planes_mongo_crud/db/password --output text --query Parameter.Value)"
+export PLANES_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/planes_mongo_crud/db/password --output text --query Parameter.Value)"
 
 exec node dist/main
